5580b31853bed9f26f4b9d7096ab9cbd6d925236
SamuelDeb <samuel.debaer@gmail.com>
2025-09-03 09:57:20 +0200

    ajout component add-produit et lister les produits front

---
+++ src/app/add-produit/add-produit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produit',
+  imports: [],
+  templateUrl: './add-produit.html',
+  styleUrl: './add-produit.css'
+})
+export class AddProduit {
+
+}
--- src/app/produits/produits.ts
+++ src/app/produits/produits.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produits',
   standalone : true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produits.html',
   styleUrl: './produits.css'
 })
 export class Produits {
+
+  produits : string [];
+
+  constructor() {
+    this.produits = ['ASUS', 'Imprimante HP', 'Smartphone Samsung', 'Smartphone Iphone'];
+  }
 
 }
